93f1c3981d130f6653a684ebfdcbd2344a0ee0f5
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-08-19 14:04:32 +0200

    add default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
   resources :movies
   resources :lists, only: [:index, :show, :new, :create] do
     # ici selon l'énoncé on place bookmark en tant qu'enfant de lists.
